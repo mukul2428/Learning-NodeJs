14c43da82049d0318cb5adca9391c0cef3f52efc
Mukul Raghav <mukulraghav28@gmail.com>
2021-07-20 12:24:24 +0530

    api

---
+++ API/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 5500;
+
+app.get("/", (req, res) => {  //this is callback
+    res.send("This is Home Page of my First Express Application");
+});
+app.get("/api", (req, res) => {           //file encoding to avoid buffer data  
+    fs.readFile(`${__dirname}/userapi.json`, "utf-8", (err, data) => {
+        res.send(data);
+    });
+    //OR
+    // res.sendFile(path.join(__dirname+'/userapi.json'));
+});
+
+app.listen(port, () => {
+    console.log(`Application started successfully on port ${port}`);
+});
